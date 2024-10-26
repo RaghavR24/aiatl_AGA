b0d0fc8f88092ad1fd68892256e3031a5a5cefcd
Raghav R <rramasamy8@gatech.edu>
2024-10-26 13:38:37 -0400

    plsss

--- neuromap/src/app/api/auth/[...nextauth]/route.ts
+++ neuromap/src/app/api/auth/[...nextauth]/route.ts
@@ -12,7 +12,7 @@ declare module "next-auth" {
   }
 }
 
-export const authOptions: AuthOptions = {
+const authOptions: AuthOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
