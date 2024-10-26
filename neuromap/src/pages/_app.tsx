50a85157346c3191e80143116db516b275c5afbd
Raghav R <rramasamy8@gatech.edu>
2024-10-26 14:52:40 -0400

    trying to fix

--- neuromap/src/pages/_app.tsx
+++ neuromap/src/pages/_app.tsx
@@ -1,7 +1,11 @@
 import { SessionProvider } from "next-auth/react"
 import { AppProps } from "next/app"
+import { Session } from "next-auth"
 
-function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+function MyApp({
+  Component,
+  pageProps: { session, ...pageProps }
+}: AppProps<{ session: Session | null }>) {
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
